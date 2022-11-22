04055d81fa7200a76800f868937ae1cc90d3468d
Mulualem Mekonnen <mulualemasfaw2321@gmail.com>
2022-11-22 17:00:07 +0300

    login api

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,7 +1,22 @@
 const User = require('../models/user.model')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_sec = "asfnjkdshf24879834583;'.f093498567*^67343@##@skdfjidsfaishcuhfksnksjfheygfusgfgfeufdsjf1654";
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
 
-exports.login = (req, res, next) => {
-    new User({ username: 'Nahom Habtamu Debalkie' }).save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err))
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: "User Not found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email: user.email }, JWT_sec);
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+  res.json({ status: "error", error: "Invalid Password" });
 }
--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,21 +1,25 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res) => {
     const { uname, fname, lname, email, password } = req.body;
-
-    var newUser = new User({
+    
+    var encryptedPassword = await bcrypt.hash(password, 10);
+    var oldUser = await User.findOne({email});
+        if(oldUser){
+           return res.json({error: "User exists"})
+        }
+   var newUser = new User({
         username: uname,
         first_name: fname,
         last_name: lname,
         profile_pic: "default-profile-pic",
         email: email,
-        password: password
+        password: encryptedPassword
     });
 
-    await newUser.save((err, user) => {
+    await newUser.save((err, User) => {
         if (err) res.send({ status: 'error' })
         res.send({ status: "ok" })
     })
-
-
 }
