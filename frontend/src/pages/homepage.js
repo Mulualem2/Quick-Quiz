57e1b7b1b0b7119b26088aac067a6201c72c3108
Nahom Habtamu <nhabtamu42@gmail.com>
2022-12-06 12:02:00 +0300

    homepage

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,36 +1,41 @@
 import React from 'react'
-import {Grid} from '@mui/material';
 
-import MediaCard from '../components/MediaCard';
-import MediaComponent from '../components/MediaComponent';
-import Media_Card from '../components/Media_Card';
+import "../styles/homepage.css"
+
 function Homepage() {
   return (
-      <Grid className='backGround'
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100vh',
-        }}
-      >
-        {/* <img src = {image}></img> */}
-        <Grid align = "center">
-        
-          <h1>Test your self with random quizzes</h1>
-        </Grid>
-        <Grid align = "center">
-          <h2>What you can do?</h2>
-        </Grid>
-        <Grid className='GridLayout'>
-          <MediaCard />
-        </Grid>
-        <Grid className='Layout'>
-        <MediaComponent />
-        </Grid>
-        <Grid className='Layout_1'>
-          <Media_Card />
-        </Grid>
-      </Grid>
+      <div>
+        <div className="container">
+        <div className="description-container">
+            <h1 className="big-slogan">THINK. ANSWER. FAST.</h1>
+            <div className="yellow-dash"></div>
+            <h3 className="what-to-do">WHAT TO DO</h3>
+            <p className="short-description-paragraph">have fun by taking different quizzes with different categories like
+                General Knowledge, Film & TV, Science, History....</p>
+            <button className="try-it-btn">TRY IT</button>
+            <div className="question-box">
+                <p className="question-category">General Knowledge</p>
+                <h3 className="question-text">What geometric shape is generally used for stop signs?</h3>
+                <div className="question-choice">
+                    <div className="question-choice-checkbox"></div>
+                    <span className="question-chioce-text">A. Hexagon</span>
+                </div>
+                <div className="question-choice">
+                    <div className="question-choice-checkbox"></div>
+                    <span className="question-chioce-text">B. Circle</span>
+                </div>
+                <div className="question-choice">
+                    <div className="question-choice-checkbox"></div>
+                    <span className="question-chioce-text">C. Circle</span>
+                </div>
+                <div className="question-choice selected-question-choice">
+                    <div className="question-choice-checkbox"></div>
+                    <span className="question-chioce-text">D. Octagon</span>
+                </div>
+            </div>
+        </div>
+    </div>
+      </div>
   );
 }
 
